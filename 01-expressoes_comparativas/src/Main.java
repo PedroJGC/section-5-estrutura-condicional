2b88e01d7fee80290bb35d9a2e0ef6f4c40903fd
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-27 18:13:12 -0300

    feat: Comparative expressions

---
+++ 01-expressoes_comparativas/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+
+        System.out.println(x > 0);
+        System.out.println(x == 3);
+        System.out.println(10 <= 30);
+        System.out.println(x != 2);
+    }
+}
