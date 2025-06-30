762f2f5e2abc375f32f66cc835c2744816a2f65d
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-30 17:18:36 -0300

    feat: 03 - Switch case

---
+++ 03-switch_case/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        String dia = switch (x) {
+            case 1 -> "Domingo";
+            case 2 -> "Segunda";
+            case 3 -> "Terça";
+            case 4 -> "Quarta";
+            case 5 -> "Quinta";
+            case 6 -> "Sexta";
+            case 7 -> "Sábado";
+            default -> "Inválido!";
+        };
+
+        System.out.println("Dia da semana: " + dia);
+
+        sc.close();
+    }
+}
